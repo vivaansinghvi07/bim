221926b27397e02df464b424b70d9b1ebe6b7e70
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2024-03-01 23:39:27 -0500

    moved the comment explaining source

--- src/input.c
+++ src/input.c
@@ -5,6 +5,12 @@
 
 static struct termios tbufsave;
 
+/*
+ *  Implementation for the two below functions taken from stty source:
+ *    https://github.com/wertarbyte/coreutils/blob/master/src/stty.c#L1162
+ *  And this reference:
+ *    https://www.scosales.com/ta/kb/100487.html
+ */
 int input_set_tty_raw(void) {
         struct termios tbuf;
         if (tcgetattr(0, &tbufsave) == -1) {
@@ -20,7 +26,6 @@ int input_set_tty_raw(void) {
 
         return tcsetattr(0, TCSANOW, &tbuf) == -1;   // 1 for error
 }
-
 int input_restore_tty(void) {
         return tcsetattr(0, TCSANOW, &tbufsave) == -1;   // 1 for error
 }
--- src/input.h
+++ src/input.h
@@ -1,12 +1,6 @@
 #ifndef EDITOR_INPUT
 #define EDITOR_INPUT
 
-/*
- *  Implementation for the two below functions taken from stty source:
- *    https://github.com/wertarbyte/coreutils/blob/master/src/stty.c#L1162
- *  And this reference:
- *    https://www.scosales.com/ta/kb/100487.html
- */
 int input_set_tty_raw(void);
 int input_restore_tty(void);
 
