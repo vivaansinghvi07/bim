636c76d23bbbacc37a463574274f2ae221f42cae
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2024-03-14 23:39:09 -0400

    another list function, more being working on rn

--- src/list.h
+++ src/list.h
@@ -8,6 +8,7 @@
 #ifndef EDITOR_LIST
 #define EDITOR_LIST
 
+// declares a new list type called <type> containing an array of items of <type>
 #define list_typedef(name, type)    \
         typedef struct {            \
                 size_t cap;         \
@@ -15,6 +16,7 @@
                 type *items;        \
         } name
 
+// add one element, <item> to <list>
 #define list_append(list, item)                                                                             \
         do {                                                                                                \
                 if ((list).cap < ++(list).len) {                                                            \
@@ -24,11 +26,21 @@
                 (list).items[(list).len - 1] = item;                                                        \
         } while (0)                                                                              
 
+// initialize a new list of type <type> and size <size> 
 #define list_init(type, size)                                     \
         (type) {                                                  \
                 (size),             /* capacity */                \
                 0,                  /*  length  */                \
                 malloc((size) * sizeof(*((type *) 0)->items))     \
         }           
+
+// assure there is enough space for <space> more items
+#define list_create_space(list, space)                                                                      \
+        do {                                                                                                \
+                while ((list).cap + space < (list).n) {                                                     \
+                        (list).cap *= 2;                                                                    \
+                        (list).items = realloc((list).items, (list).cap * sizeof(*(list).items));           \
+                }                                                                                           \
+        } while(0)
 
 #endif
