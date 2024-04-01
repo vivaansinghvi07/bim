16dc3446000761226e997cf02458f9be860fe95d
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2024-04-01 19:19:54 -0400

    added bindings to cycle gradient directions

--- src/input/normal.c
+++ src/input/normal.c
@@ -8,6 +8,9 @@
 #define C_MOVE_DOWN 's'
 #define C_MOVE_RIGHT 'd'
 
+#define C_GRAD_ANG_INCRE '['
+#define C_GRAD_ANG_DECRE ']'
+
 void handle_c_move_up(file_buf *buf) {
         if (buf->cursor_line > 1) {
                 if (buf->screen_top_line == buf->cursor_line) { 
@@ -36,6 +39,14 @@ void handle_normal_input(editor_state_t *state, char c) {
         switch (c) {
                 case C_MOVE_UP: handle_c_move_up(buf); break;
                 case C_MOVE_DOWN: handle_c_move_down(buf); break;
+                case C_GRAD_ANG_INCRE:
+                        state->display_state.gradient_angle++;
+                        state->display_state.gradient_angle %= 8;
+                        break;
+                case C_GRAD_ANG_DECRE: 
+                        state->display_state.gradient_angle += 7;
+                        state->display_state.gradient_angle %= 8;
+                        break;
         }
 
         display_buffer(state);
